8398bb59940b154ede584daed649feb5367b5c16
Jovo <sunjkajovo@gmail.com>
2019-11-20 22:02:53 +0100

    Initial project

---
+++ src/test/java/com/rmj/PaymentConcentrator/PaymentConcentratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rmj.PaymentConcentrator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentConcentratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
