e9c505849864577f8d0fb05841d6755d78b02aac
IgorResman <39412165+IgorResman@users.noreply.github.com>
2020-01-25 13:36:03 +0100

    Popravljen unos podataka za banku

--- Bank/SEP-Banka/src/main/java/com/rmj/SEP/Banka/controller/BankController.java
+++ Bank/SEP-Banka/src/main/java/com/rmj/SEP/Banka/controller/BankController.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -24,29 +25,65 @@ public class BankController {
     {
         List<BankAccount> accounts = new ArrayList<>();
         accounts = serviceRepo.findAll();
+        LocalDate date = LocalDate.now();
+        String[] dateSplit = user.getExpDate().split("/");
+        int year = Integer.parseInt(dateSplit[1]);
+        int month = Integer.parseInt(dateSplit[0]);
+        
+        int aa = date.getYear()%100;
 
         if(accounts.size()>0)
         {
             for(int i = 0; i < accounts.size();i++)
             {
-                if(accounts.get(i).getCardNumber() == user.getCardNumber() && accounts.get(i).getPin() == user.getPin())
+                if(accounts.get(i).getCardNumber() == user.getCardNumber() && accounts.get(i).getSecurityCode() == user.getSecurityCode() && user.getCardHolder().equals(accounts.get(i).getCardHolder()))
                 {
-                    if(accounts.get(i).getAmount()>= user.getAmount())
-                    {
-                    	double pom = 0;
-                    	
-                    	pom = accounts.get(i).getAmount() - user.getAmount();
-                    	
-                    	accounts.get(i).setAmount(pom);
-                    	
-                    	serviceRepo.save(accounts.get(i));
-                    	
-                        return ResponseEntity.status(HttpStatus.OK).body("Transaction is acceptable!");
-                    }
-                    else
-                    {
-                        return ResponseEntity.status(HttpStatus.OK).body("User does not have enough assets");
-                    }
+                	if(year > aa)
+                	{
+                		if(accounts.get(i).getAmount()>= user.getAmount())
+                        {
+                        	double pom = 0;
+                        	
+                        	pom = accounts.get(i).getAmount() - user.getAmount();
+                        	
+                        	accounts.get(i).setAmount(pom);
+                        	
+                        	serviceRepo.save(accounts.get(i));
+                        	
+                            return ResponseEntity.status(HttpStatus.OK).body("Transaction is acceptable!");
+                        }
+                        else
+                        {
+                            return ResponseEntity.status(HttpStatus.OK).body("User does not have enough assets");
+                        }   
+                	}
+                	else if(year == aa)
+                	{
+                		if(month > date.getMonthValue())
+                		{
+                			if(accounts.get(i).getAmount()>= user.getAmount())
+                            {
+                            	double pom = 0;
+                            	
+                            	pom = accounts.get(i).getAmount() - user.getAmount();
+                            	
+                            	accounts.get(i).setAmount(pom);
+                            	
+                            	serviceRepo.save(accounts.get(i));
+                            	
+                                return ResponseEntity.status(HttpStatus.OK).body("Transaction is acceptable!");
+                            }
+                            else
+                            {
+                                return ResponseEntity.status(HttpStatus.OK).body("User does not have enough assets");
+                            }   
+                		}
+                		else
+                			return ResponseEntity.status(HttpStatus.OK).body("Users card has expired");
+                	}
+                	else
+            			return ResponseEntity.status(HttpStatus.OK).body("Users card has expired");
+                		                 
                 }
                 else
                 {
--- card_payment_microservice/CardPaymentMicroservice/src/main/java/com/rmj/CardPaymentMicroservice/dto/CardNumberAndPinDTO.java
+++ card_payment_microservice/CardPaymentMicroservice/src/main/java/com/rmj/CardPaymentMicroservice/dto/CardNumberAndPinDTO.java
@@ -2,15 +2,38 @@ package com.rmj.CardPaymentMicroservice.dto;
 
 public class CardNumberAndPinDTO {
 	private int cardNumber;
-	private int pin;
+	private int securityCode;
+	private String cardHolder;
+	private String expirationDate;
 	
 	public CardNumberAndPinDTO() {
 		
 	}
 	
-	public CardNumberAndPinDTO(int cardNumber, int pin) {
+	public CardNumberAndPinDTO(int cardNumber, int securityCode,String cardHolder,String expirationDate) {
 		this.cardNumber = cardNumber;
-		this.pin = pin;
+		this.securityCode = securityCode;
+		this.cardHolder = cardHolder;
+		this.expirationDate = expirationDate;
+	}
+	public String getCardHolder()
+	{
+		return cardHolder;
+	}
+	
+	public void setCardHolder(String cardHolder)
+	{
+		this.cardHolder = cardHolder;
+	}
+	
+	public String getExpirationDate()
+	{
+		return this.expirationDate;
+	}
+	
+	public void setExpirationDate(String expirationDate)
+	{
+		this.expirationDate = expirationDate;
 	}
 
 	public int getCardNumber() {
@@ -21,12 +44,12 @@ public class CardNumberAndPinDTO {
 		this.cardNumber = cardNumber;
 	}
 
-	public int getPin() {
-		return pin;
+	public int getSecurityCode() {
+		return securityCode;
 	}
 
-	public void setPin(int pin) {
-		this.pin = pin;
+	public void setSecurityCode(int securityCode) {
+		this.securityCode = securityCode;
 	}
 
 }
