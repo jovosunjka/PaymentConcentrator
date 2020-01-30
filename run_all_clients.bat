b90916cbde28ee9d22a71c3baee55e8eaedb5d65
Jovo <sunjkajovo@gmail.com>
2020-01-30 22:08:28 +0100

    commit

--- run_all_clients.bat
+++ run_all_clients.bat
@@ -7,11 +7,11 @@ echo payment-microservice-frontend is running!
 :: start "PayPal_frontend" npm start --prefix pay_pal_microservice\PayPal
 :: echo PayPal_frontend is running!
 
-start "BitCoinFront" npm start --prefix bitcoin_microservice\BitCoinFront
-echo BitCoinFront is running!
+:: start "BitCoinFront" npm start --prefix bitcoin_microservice\BitCoinFront
+:: echo BitCoinFront is running!
 
-:: start "card_payment_microservice_BankFront" npm start --prefix card_payment_microservice\BankFront
-:: echo card_payment_microservice_BankFront is running!
+start "card_payment_microservice_BankFront" npm start --prefix card_payment_microservice\BankFront
+echo card_payment_microservice_BankFront is running!
 
 start "science-center-frontend" npm start --prefix ..\..\science_center_workspace\ScienceCenterRepository\science-center-frontend
 echo science-center-frontend is running!
